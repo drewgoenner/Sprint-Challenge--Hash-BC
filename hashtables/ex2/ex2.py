d9a3ab3588235728679326a2dae449ca8b3516c0
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2020-02-14 12:01:21 -0600

    completed hashtables ex2, works

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 class Ticket:
@@ -20,4 +18,20 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
 
-    pass
+    #go through the tickets and insert the source and destinations into the ht
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+        current_location = "NONE"
+
+    # loop through to prepare to make the path
+    for i in range(length):
+    # once it finds the source(0), start by retrieving the none in source and start the route
+        route[i] = hash_table_retrieve(hashtable, current_location)
+        current_location = route[i]
+
+
+    # pop the last item in the array to get rid of "NONE"
+    route.pop()
+    #return route
+    return route
